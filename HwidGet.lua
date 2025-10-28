28e7f8d7ccd6029f9ed3fd932c2b30c4e8eb31f9
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-28 15:41:52 +0700

    Update HwidGet.lua

--- HwidGet.lua
+++ HwidGet.lua
@@ -1,17 +1,14 @@
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local HWID = gethwid() or game:GetService("RbxAnalyticsService"):GetClientId()
+local player = Players.LocalPlayer
+local HWID = gethwid()
 
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-
-print("║")
+print(("\n"):rep(150))
 print("║")
 print("║               SPACE HUB PREMIUM")
 print("║                   Your HWID:")
 print("║                  ".. HWID)
-print("║")                                             
-print("║\n\n\n\n\n\n\n\n\n\n")
+print("║")
+print(("\n"):rep(10))
 
 setclipboard(HWID)
-LocalPlayer:Kick("Your HWID: (" .. HWID .. ") \nAUTO COPIED TO THE CLIPBOARD!")
 loadstring(game:HttpGet("https://gist.githubusercontent.com/ago106/d9360cdac8557282efd6b446bf9fb158/raw/"))()
